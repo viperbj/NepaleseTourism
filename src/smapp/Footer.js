40b5801601c66b53b4968e8b207f1d7891458af8
viperbj <bijaykkarn@gmail.com>
2021-08-31 08:22:09 +0545

    Contact Updated

--- src/smapp/Footer.js
+++ src/smapp/Footer.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import "./smapp.css"
 
 export default function Footer() {
     return (
-      <>
-      <div className="footeer">
-       
-          
-     
-       <strong>Copyright &copy; 2020-2021 <a class= "rom" href="/">Nepalese Tourism</a>.</strong> All rights reserved.
-     </div>
-      </>
+    <>
+        {/* <!-- footer section --> */}
+          <footer>
+            <div className="footer">
+              <div className="row">
+
+                <svg className="svgcolor-light" preserveAspectRatio="none" viewBox="0 0 100 102" height="100" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0 L50 100 L100 0 Z"></path>
+                </svg>
+
+                <div className="textleft">
+                  <h2>Nepalese Tourism</h2>
+                  <div className="wow fadeInUp" data-wow-delay="0.3s">
+                    <p>“We, Nepalese Tourism desired to served many customers <br/>downtown Kathmandu Valley as well as all Nepal<br/> You can also be one of them.</p>
+                      
+                  </div>
+               
+                    
+                      <ul className="social-icon">
+                        <li><a href="https://www.facebook.com/chakrafoundations" target="_blank" className="fab fa-facebook wow bounceIn" data-wow-delay="0.9s"></a></li>
+                        <li><a href="https://twitter.com/" target="_blank" className="fab fa-twitter wow bounceIn" data-wow-delay="1.2s"></a></li>
+                        <li><a href="https://www.instagram.com/chakrafoundations/" target="_blank" className="fab fa-instagram wow bounceIn" data-wow-delay="1.4s"></a></li>
+                        <li><a href="https://chakrafoundations.business.site/" target="_blank" className="fab fa-google wow bounceIn" data-wow-delay="1.6s"></a></li>
+                      </ul>
+                      <p className="copyright-text">Copyright &copy; 2021 Nepalese Tourism<br/></p>
+                </div>
+                  <br/>
+
+              </div>
+            </div>
+          </footer>
+    </>
     )
 }
--- src/smapp/Nav.js
+++ src/smapp/Nav.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import './smapp.css';
+import animal from './animal.jpg'
 export default function nav() {
     return (
         <>
        <div className="logobar">
             <a href="/"className="mainlogo">NEPAL TOURISM</a>
-            </div>
+        </div>
             <div className="navbar">
-            <ul className="nav">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#destination">Destination</a></li>
-                <li><a href="#tour">Tours</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <ul className="nav">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#destination">Destination</a></li>
+                    <li><a href="#tour">Tours</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
         
-          </div>
+            </div>
            
-       <div className="home-space" id="home"></div>
+        <div className="home-space" id="home">
+            <a href="#about" className="btn btn-lg btn-success smoothScroll wow fadeInUp" data-wow-delay="0.8s">Let's begin</a>
+        	<a href="#" data-toggle="modal" data-target="#modal1" className="btn btn-lg btn-success smoothScroll wow fadeInUp" data-wow-delay="1.0s">Notify me!</a>
+        </div>
        
        <div className="about-space"id="about">
        <div className="text-part">
@@ -40,12 +44,75 @@ export default function nav() {
        </div>
        <div className="contact-space"id="contact">
        <div className="text-part">
-               <h1>contact page</h1>
-              
-           </div>
-       </div>
-    
-    
+       <>
+            <div className="cont-back">
+            
+            <img src={animal} className="img-rounded" alt="Animal Photo" width="40%" height="326"></img>
+                        
+
+            <div className="contactForm">
+                <form>
+                    <div>
+                        <h1 className="section-title">COMMENT <span>US</span></h1>
+                    </div>
+                    <div className="inputBox">
+                        <input type="text" name="name" required />
+                        <span>Name</span>
+                    </div>
+                    <div className="inputBox">
+                        <input type="email" name="email" required />
+                        <span>Email</span>
+                    </div>
+                    <br/>
+                    <div className="inputBo">
+                                      
+                        <textarea name="comment" cols="64" rows="5" placeholder="Comment Here"/>
+                        
+                    </div>
+                    <div className="inputBox">
+                        <input type="submit" name="" value="Comment" />
+                        <button type="reset" value="Clear" >Clear</button>
+                    </div>
+                </form>
+            </div>
+
+            <section id="contact">
+                <div className="contact container">
+                    
+                    <div className="contact-items">
+                    <div className="contact-item">
+                            <i className="icon fas fa-map-marker-alt fa-3x"></i>
+                            <div className="contact-info">
+                                <h2>ADDRESS</h2>
+                                <a href="https://goo.gl/maps/GwjSCuE7KhzcZXK26" className="location"><h3>Baneshwar, Kathmandu, Nepal</h3></a>
+                            </div>
+                        </div>
+
+                        <div className="contact-item">
+                            <i className="icon fas fa-phone-volume fa-3x"></i>
+                            <div className="contact-info">
+                                <h2>PHONE</h2>
+                                <h3>+977 9880 085 851</h3>
+                            </div>
+                        </div>
+
+                        <div className="contact-item">
+                            <i className="icon fas fa-envelope-open-text fa-3x"></i>
+                            <div className="contact-info">
+                                <h2>EMAIL</h2>
+                                <h3>nplztourism@gmail.com</h3>
+                            </div>
+                        </div>
+                        <br/><br/><br/><br/>
+                    </div>
+                </div>
+            </section>
+            <br/><br/><br/>
+
+            </div>
+        </>      
+        </div>
+       </div>  
        
     </>
     )
